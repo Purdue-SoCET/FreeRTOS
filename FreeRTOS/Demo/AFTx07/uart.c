cfa54bfbc6e11dabe938d43e0c9a2066b743243c
Po-Chin Yang <yang2775@asicfab.ecn.purdue.edu>
2025-10-08 14:56:01 -0400

    Adding magic print able to run on verilator

--- FreeRTOS/Demo/AFTx07/uart.c
+++ FreeRTOS/Demo/AFTx07/uart.c
@@ -8,16 +8,27 @@ static UARTRegBlk *uart = (UARTRegBlk *) UART_BASE;
 /* Set up UART BAUD_RATE */
 
 
-
+/*
 void uart_setup() 
 {
     uart->rxstate = (BAUD_CYCLES / 16) << 16;
     uart->txstate = BAUD_CYCLES << 16;
 }
 
-/* Send one byte to UART TX */
 void uart_sendbyte(char onechar)
 {
     uart->txdata = (uint32_t) onechar | 1<<24;
     while (!(uart->txstate & 0x1));
+}
+*/
+
+void uart_setup( void )
+{
+    
+}
+
+void uart_sendbyte( char onechar )
+{
+    volatile char *MAGIC_ADDR = (volatile char *)0xB0000000;
+    *MAGIC_ADDR = onechar;
 }
